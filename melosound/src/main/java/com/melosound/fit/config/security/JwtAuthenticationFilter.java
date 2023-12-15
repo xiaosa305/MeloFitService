99a81f3ec0db6620d2c1f08b3328c7791a28793f
xiaosa305 <callmexiaosa@gmail.com>
2023-12-15 13:56:09 +0800

    test

--- melosound/src/main/java/com/melosound/fit/config/security/JwtAuthenticationFilter.java
+++ melosound/src/main/java/com/melosound/fit/config/security/JwtAuthenticationFilter.java
@@ -1,37 +1,26 @@
 package com.melosound.fit.config.security;
 
 import java.io.IOException;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
-
 import javax.annotation.Resource;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.melosound.fit.domain.CustomUser;
-import com.melosound.fit.domain.cusenum.ResponseCode;
-import com.melosound.fit.domain.response.ApiResponseBuilder;
 import com.melosound.fit.service.impl.MeloUserServiceImpl;
 import com.melosound.fit.utils.JwtUtils;
 import com.melosound.fit.utils.RedisKeyUtil;
-
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSONUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.netty.util.internal.StringUtil;
 import lombok.extern.slf4j.Slf4j;
